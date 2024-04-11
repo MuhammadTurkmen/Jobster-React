283626e54709e15595b0b4230a9bc61ff81770bc
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-11 14:56:05 +0430

    init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import Landing from "./pages/Landing";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: red;
+  color: white;
+  font-size: 2rem;
+`;
 
 function App() {
   return (
