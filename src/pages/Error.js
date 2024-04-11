4da3d534fe898147e1ea55bbd474ca5da33194b4
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-11 20:46:51 +0430

    init

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -4,7 +4,13 @@ import Wrapper from "../assets/wrappers/ErrorPage";
 import React from "react";
 
 const Error = () => {
-  return <Wrapper className="full-page"></Wrapper>;
+  return (
+    <Wrapper className="full-page">
+      <div>
+        <img src={img} alt="not found" />
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
