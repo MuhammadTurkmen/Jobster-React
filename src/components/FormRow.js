82bb9c2288e0c47c590c646414feef08ecd34837
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-11 13:35:59 +0430

    init

--- src/components/FormRow.js
+++ src/components/FormRow.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-const FormRow = ({ type, name, value, handleChange, label }) => {
+const FormRow = ({ type, name, value, handleChange, labelText }) => {
   return (
     <div className="form-row">
-      <label htmlFor="name" className="form-label">
-        name
+      <label htmlFor={name} className="form-label">
+        {name}
       </label>
       <input
-        type="text"
-        name="name"
-        value={values.name}
+        type={type}
+        name={name}
+        value={value}
         onChange={handleChange}
         className="form-input"
       />
