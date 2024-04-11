f8fea4a764ed99007d7e556b4afe42fe82eb4946
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-11 11:00:39 +0430

    init

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,6 +3,12 @@ import { Logo } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
 import React from "react";
 
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+};
+
 const Register = () => {
   return <div>Register</div>;
 };
