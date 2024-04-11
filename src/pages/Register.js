ef83744086b41246bb2c4f2e95a35aa27e8647c1
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-11 14:02:32 +0430

    init

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -16,7 +16,7 @@ const Register = () => {
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    console.log(`${name}:${value}`);
+    setValues({ ...values, [name]: value });
   };
   const onSubmit = (e) => {
     e.preventDefault();
