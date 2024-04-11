a3f5bd735051b3484601a926356be19ebc3555f6
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-11 20:44:21 +0430

    init

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import img from "../assets/images/not-found.svg";
+import Wrapper from "../assets/wrappers/ErrorPage";
 import React from "react";
 
 const Error = () => {
