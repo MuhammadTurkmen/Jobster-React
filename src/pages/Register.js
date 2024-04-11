e2d00fc2d31f98264646bd5d638874f1ca241232
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-11 10:57:33 +0430

    init

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,3 +1,6 @@
+import { useState, useEffect } from "react";
+import { Logo } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
 import React from "react";
 
 const Register = () => {
