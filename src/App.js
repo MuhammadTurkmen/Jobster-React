a6e598c57635bbb92c665b6e20089ed1b800899f
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-11 15:06:29 +0430

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import Landing from "./pages/Landing";
 import styled from "styled-components";
 
-const Button = styled.button`
-  background: red;
-  color: white;
-  font-size: 2rem;
-`;
-const SecondButton = styled.button`
-  background: blue;
-  color: yellow;
-  font-size: 3rem;
-`;
-
 function App() {
   return (
     <div>
-      <Button>Click me</Button>
-      <SecondButton>Click me</SecondButton>
       <Landing />
     </div>
   );
