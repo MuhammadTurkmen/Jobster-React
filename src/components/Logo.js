d43130e7618d71880d9a578197645f2a8c049b53
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-11 17:06:46 +0430

    init

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,1 +1,9 @@
-import img from "../assets/images/logo.svg";
+import logo from "../assets/images/logo.svg";
+
+import React from "react";
+
+const Logo = () => {
+  return <img src={logo} alt="jobster logo" className="logo" />;
+};
+
+export default Logo;
