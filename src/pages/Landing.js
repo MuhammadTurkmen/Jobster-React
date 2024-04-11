68a018aa15f95b0227bf31912fe952e6df2f6e59
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-11 15:10:09 +0430

    init

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -32,6 +32,10 @@ const Wrapper = styled.main`
   nav {
     width: var(--fluid-width);
     max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
   }
 `;
 
