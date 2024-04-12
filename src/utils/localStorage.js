4a91d280b55538d17ccd41e0e1278174c329f18d
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-12 12:11:46 +0430

    init

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -9,4 +9,5 @@ export const removeUserFromLocalStorage = () => {
 export const getUserFromLocalStorage = () => {
   const result = localStorage.getItem("user");
   const user = result ? JSON.parse(result) : null;
+  return user;
 };
