a7f27fdfc8109f7939139d5cd1892a036e9e5fc9
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-12 11:42:12 +0430

    init

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,7 +39,7 @@ const Navbar = () => {
             <button
               type="button"
               className="dropdown-btn"
-              onClick={() => console.log("logout user")}
+              onClick={() => dispatch(logoutUser())}
             >
               logout
             </button>
