7906a427a04ecb53ef4fa28cec4747bc355107eb
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-12 11:07:49 +0430

    init

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,11 @@ import Wrapper from "../assets/wrappers/Navbar";
 import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
 import Logo from "./Logo";
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Navbar = () => {
+  const { user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
   return (
     <Wrapper>
       <h2>Navbar</h2>
