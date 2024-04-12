218aa338014123ec1a88ca28b3b08a9f86126c34
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-12 10:33:09 +0430

    init

--- src/App.js
+++ src/App.js
@@ -14,7 +14,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SharedLayout />} />
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-jobs" element={<AddJobs />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
         <Route path="landing" element={<Landing />} />
         <Route path="register" element={<Register />} />
         <Route path="*" element={<Error />} />
---
+++ src/pages/dashboard/AddJobs.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddJobs = () => {
+  return <h1>AddJobs</h1>;
+};
+
+export default AddJobs;
