8bb2ac3f7f318318895eca9038f7ae2054f404ba
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-12 11:06:49 +0430

    init

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/Navbar";
-import { FaHome } from "react-icons/fa";
+import Wrapper from "../assets/wrappers/Navbar";
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import Logo from "./Logo";
+import { useState } from "react";
 
 const Navbar = () => {
   return (
     <Wrapper>
       <h2>Navbar</h2>
-      <FaHome />
     </Wrapper>
   );
 };
