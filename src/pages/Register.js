3f24dbf51e6e9e648a309181ca33af9f6cf1fb8e
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-12 09:03:07 +0430

    init

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,6 +3,7 @@ import { Logo, FormRow } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
 import { toast } from "react-toastify";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const initialState = {
   name: "",
@@ -13,6 +14,7 @@ const initialState = {
 
 const Register = () => {
   const [values, setValues] = useState(initialState);
+  const { user, isLoading } = useSelector((store) => store.user);
 
   const handleChange = (e) => {
     const name = e.target.name;
