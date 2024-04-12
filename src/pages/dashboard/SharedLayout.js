47a888c8cd5bc3d33a681d25c76a0fce3e0831f8
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-12 10:41:40 +0430

    init

--- src/pages/dashboard/SharedLayout.js
+++ src/pages/dashboard/SharedLayout.js
@@ -3,7 +3,11 @@ import { Outlet } from "react-router-dom";
 import { BigSidebar, Navbar, SmallSidebar } from "../../components";
 import Wrapper from "../../assets/wrappers/SharedLayout";
 const SharedLayout = () => {
-  return <Wrapper>SharedLayout</Wrapper>;
+  return (
+    <Wrapper>
+      <main className="dashboard"></main>
+    </Wrapper>
+  );
 };
 
 export default SharedLayout;
