0c1d9a1ff2ca7bbe50022036297a5dc6ebd62196
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-12 10:51:58 +0430

    init

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Wrapper from "../assets/wrappers/Navbar";
 
 const Navbar = () => {
-  return <h2>Navbar</h2>;
+  return (
+    <Wrapper>
+      <h2>Navbar</h2>
+    </Wrapper>
+  );
 };
 
 export default Navbar;
