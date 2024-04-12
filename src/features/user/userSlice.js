b94dd4a740eff6298303c66fef8f264884654e11
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-12 11:21:07 +0430

    init

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -22,7 +22,12 @@ export const registerUser = createAsyncThunk(
 export const loginUser = createAsyncThunk(
   "user/loginUser",
   async (user, thunkAPI) => {
-    console.log(`Login User : ${JSON.stringify(user)}`);
+    try {
+      const resp = await customFetch.post("/auth/register", user);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
   }
 );
 
