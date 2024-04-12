da27d931e86ac9b63cfd49de6671501576023f98
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-12 10:33:58 +0430

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import Error from "./Error";
 import Register from "./Register";
 import Landing from "./Landing";
-import Dashboard from "./Dashboard";
 
-export { Error, Register, Landing, Dashboard };
+export { Error, Register, Landing };
