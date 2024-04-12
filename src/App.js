5778c25070faaa2656dc095e25b19b51d00fe7ac
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-12 21:01:18 +0430

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Landing, Error, Dashboard, Register } from "./pages";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
