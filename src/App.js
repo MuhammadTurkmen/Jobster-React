963cfca6a154df42aebf409cac97f46fc2a0b72c
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-04-13 17:40:05 +0430

    init

--- src/App.js
+++ src/App.js
@@ -14,7 +14,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
           <Route index element={<Stats />} />
           <Route path="all-jobs" element={<AllJobs />} />
           <Route path="add-jobs" element={<AddJobs />} />
